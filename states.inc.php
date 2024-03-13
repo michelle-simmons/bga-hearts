bb69b1f24e2de3fa5aa08cc89bc3f39cf0871cd0
Michelle Simmons <michellecsim@gmail.com>
2024-03-13 14:58:56 -0600

    fill out state machine

--- states.inc.php
+++ states.inc.php
@@ -62,39 +62,70 @@ $machinestates = array(
     "transitions" => array("" => 2)
   ),
 
-  // Note: ID=2 => your first state
-
+  /// New hand
   2 => array(
-    "name" => "playerTurn",
-    "description" => clienttranslate('${actplayer} must play a card or pass'),
-    "descriptionmyturn" => clienttranslate('${you} must play a card or pass'),
-    "type" => "activeplayer",
-    "possibleactions" => array("playCard", "pass"),
-    "transitions" => array("playCard" => 2, "pass" => 2)
+    "name" => "newHand",
+    "description" => "",
+    "type" => "game",
+    "action" => "stNewHand",
+    "updateGameProgression" => true,
+    "transitions" => array("" => 30)
   ),
 
-  /*
-    Examples:
+  21 => array(
+    "name" => "giveCards",
+    "description" => clienttranslate('Some players must choose 3 cards to give to ${direction}'),
+    "descriptionmyturn" => clienttranslate('${you} must choose 3 cards to give to ${direction}'),
+    "type" => "multipleactiveplayer",
+    "action" => "stGiveCards",
+    "args" => "argGiveCards",
+    "possibleactions" => array("giveCards"),
+    "transitions" => array("giveCards" => 22, "skip" => 22)
+  ),
 
-    2 => array(
-      "name" => "nextPlayer",
-      "description" => '',
-      "type" => "game",
-      "action" => "stNextPlayer",
-      "updateGameProgression" => true,
-      "transitions" => array( "endGame" => 99, "nextPlayer" => 10 )
-    ),
+  22 => array(
+    "name" => "takeCards",
+    "description" => "",
+    "type" => "game",
+    "action" => "stTakeCards",
+    "transitions" => array("startHand" => 30, "skip" => 30)
+  ),
 
-    10 => array(
-      "name" => "playerTurn",
-      "description" => clienttranslate('${actplayer} must play a card or pass'),
-      "descriptionmyturn" => clienttranslate('${you} must play a card or pass'),
-      "type" => "activeplayer",
-      "possibleactions" => array( "playCard", "pass" ),
-      "transitions" => array( "playCard" => 2, "pass" => 2 )
-    ),
 
-*/
+  // Trick
+
+  30 => array(
+    "name" => "newTrick",
+    "description" => "",
+    "type" => "game",
+    "action" => "stNewTrick",
+    "transitions" => array("" => 31)
+  ),
+  31 => array(
+    "name" => "playerTurn",
+    "description" => clienttranslate('${actplayer} must play a card'),
+    "descriptionmyturn" => clienttranslate('${you} must play a card'),
+    "type" => "activeplayer",
+    "possibleactions" => array("playCard"),
+    "transitions" => array("playCard" => 32)
+  ),
+  32 => array(
+    "name" => "nextPlayer",
+    "description" => "",
+    "type" => "game",
+    "action" => "stNextPlayer",
+    "transitions" => array("nextPlayer" => 31, "nextTrick" => 30, "endHand" => 40)
+  ),
+
+
+  // End of the hand (scoring, etc...)
+  40 => array(
+    "name" => "endHand",
+    "description" => "",
+    "type" => "game",
+    "action" => "stEndHand",
+    "transitions" => array("nextHand" => 2, "endGame" => 99)
+  ),
 
   // Final state.
   // Please do not modify (and do not overload action/args methods).
