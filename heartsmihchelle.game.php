b57da05cff26e1121ae4203b95eb0379ad26a734
Michelle Simmons <michellecsim@gmail.com>
2024-03-13 14:28:45 -0600

    define game action function for starting the next player turn

--- heartsmihchelle.game.php
+++ heartsmihchelle.game.php
@@ -255,6 +255,31 @@ class heartsmihchelle extends Table
     $this->gamestate->nextState("");
   }
 
+  function stNextPlayer()
+  {
+    // Active next player OR end the trick and go to the next trick OR end the hand
+    if ($this->cards->countCardInLocation('cardsontable') == 4) {
+      // This is the end of the trick
+      // Move all cards to "cardswon" of the given player
+      $best_value_player_id = self::activeNextPlayer(); // TODO figure out winner of trick
+      $this->cards->moveAllCardsInLocation('cardsontable', 'cardswon', null, $best_value_player_id);
+
+      if ($this->cards->countCardInLocation('hand') == 0) {
+        // End of the hand
+        $this->gamestate->nextState("endHand");
+      } else {
+        // End of the trick
+        $this->gamestate->nextState("nextTrick");
+      }
+    } else {
+      // Standard case (not the end of the trick)
+      // => just active the next player
+      $player_id = self::activeNextPlayer();
+      self::giveExtraTime($player_id);
+      $this->gamestate->nextState('nextPlayer');
+    }
+  }
+
   //////////////////////////////////////////////////////////////////////////////
   //////////// Zombie
   ////////////
