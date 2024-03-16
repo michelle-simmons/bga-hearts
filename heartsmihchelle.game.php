0143862d062164ed7b3283f43df99ed6f989a7df
Michelle Simmons <michellecsim@gmail.com>
2024-03-16 15:40:56 -0600

    remove debugging example

--- heartsmihchelle.game.php
+++ heartsmihchelle.game.php
@@ -273,8 +273,6 @@ class heartsmihchelle extends Table
       // Move all cards to "cardswon" of the given player
       $currentTrickSuit = self::getGameStateValue('trickSuit');
       $currentTrickCards = $this->cards->getCardsInLocation('cardsontable');
-      // $value = var_export($currentTrickCards, true);
-      // throw new BgaUserException('current trick cards: '.$value);
       $highestValueInSuit = null;
       $trickWinnerId = null;
       foreach ($currentTrickCards as $card) {
