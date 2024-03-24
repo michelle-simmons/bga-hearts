5361cf253d99324f364d891159d30d692de84922
Michelle Simmons <michellecsim@gmail.com>
2024-03-24 12:56:51 -0600

    set up framework to autoload sharedcode test stubs

---
+++ modules/autoload.php
@@ -0,0 +1,16 @@
+<?php
+
+define("APP_GAMEMODULE_PATH", getenv('APP_GAMEMODULE_PATH'));
+
+spl_autoload_register(function ($class_name) {
+  switch ($class_name) {
+    case "APP_GameClass":
+      //var_dump($class_name);
+      //var_dump(APP_GAMEMODULE_PATH);
+      include APP_GAMEMODULE_PATH . "/module/table/table.game.php";
+      break;
+    default:
+      include $class_name . ".php";
+      break;
+  }
+});
